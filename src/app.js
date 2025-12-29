a6260c3de5f5be9e50ba8bdbac728c5d4bd1cc74
cppNut <mj9425326498@gmail.com>
2025-12-29 13:24:59 +0530

    util code

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+const app=express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+
+}))
+
+//or normally app.use(cors())
+
+app.use(express.json({limit: "16kb"}))  // json jo aa rha hai uska kya limit hai 
+app.use(express.urlencoded({extended: true, limit: "16kb"}))    //url me kahi pr / hai kahi ? , kahi %20 , to uske liye
+app.use(express.static("public"))   // files, assets public me 
+app.use(cookieParser())
+
+export {app}
--- src/index.js
+++ src/index.js
@@ -6,6 +6,20 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=>{
+
+    app.on("error",(error)=>{
+        console.log("Error : ",error);
+        throw error;
+    })
+
+    app.listen(process.env.PORT || 8000 , ()=>{
+        console.log(`App running on PORT : ${process.env.PORT || 8000}`)
+    })
+})
+.catch((err)=>{
+    console.log("MongoDB Connection Failed !!!")
+})
 
 
 /*
