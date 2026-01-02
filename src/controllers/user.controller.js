ef99c4b7bd15e8eddcb2894022a28a40277ce26c
cppNut <mj9425326498@gmail.com>
2026-01-02 16:59:56 +0530

    added getUserChannelProfile function to retrieve user channel details, including subscriber counts and subscription status (using aggregation pipilines)

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -373,6 +373,79 @@ const updateUserCoverImage = asyncHandler(async(req,res)=>{
     .json(
         new ApiResponse(200,user,"CoverImage Updated Successfully")
     )
+})
+
+const getUserChannelProfile = asyncHandler(async(req,res)=>{
+
+    const {username}=req.params
+
+    if(!username){
+        throw new ApiError(400,"Username is Missing")
+    }
+
+    const channel = await User.aggregate([
+        {
+            $match:{
+                username:username?.toLowerCase()
+            }
+        },// stage 1 (finding the specific user in the Users collection)
+        {
+            $lookup:{
+                from:"subscriptions",
+                localField:"_id",
+                foreignField:"channel",
+                as:"subscribers"
+            }
+        },//stage 2 (join getting the subscribers of X)
+        {
+            $lookup:{
+                from:"subscriptions",
+                localField:"_id",
+                foreignField:"subscriber",
+                as:"subscribedTo"
+            }
+        },//stage 3 (join krke getting who X follow )
+        {
+            $addFields:{
+                subscribersCount:{
+                    $size:"$subscribers"
+                },
+                channelsSubscribedToCount:{
+                    $size:"$subscribedTo"
+                },
+                isSubscribed:{
+                    $cond:{
+                        if:{$in: [req.user?._id,"$subscribers.subscriber"]},
+                        then:true,
+                        else:false
+                    }
+                }
+            }
+        },//stage 4 (This stage turns those lists into simple numbers and a "Yes/No" answer.)
+        {
+            $project:{
+                fullname:1,
+                username:1,
+                subscribersCount:1,
+                channelsSubscribedToCount:1,
+                isSubscribed:1,
+                avatar:1,
+                coverImage:1,
+                email:1
+            }
+        }//stage 5 (Everything else: Automatically discarded. This keeps your API response fast and secure)
+    ])
+
+    console.log("All the data of the channel user: \n",channel)
+
+    if(!channel?.length){
+        throw new ApiError(404,"Channel does not exists")
+    }
+
+    return res.status(200)
+    .json(
+        new ApiResponse(200,channel[0],"User channel fetched successfully")
+    )
 
 })
 
@@ -385,5 +458,5 @@ export {registerUser
     ,updateAccountDetailed
     ,updateUserAvatar
     ,updateUserCoverImage
-
+    ,getUserChannelProfile
 }
